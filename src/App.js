2f969b60435be576544e730c0f4af9a34d3cc5ee
Ostap Shopyak <90937268+ssshopyak@users.noreply.github.com>
2022-11-21 15:27:35 +0200

    qqq

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Calendar from 'react-calendar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="LeftMenu">
+        <Calendar/>
+      </div>
+      <div className='RightMenu'>
+        <div className='Buttons'>button</div>
+        <div className='Workers'>workers</div>
+        <div className='Time'>graphik</div>
+      </div>
     </div>
   );
 }
